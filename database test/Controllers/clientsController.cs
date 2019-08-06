b56406e037a09981e46e7990a3efdff4debbc4f4
Mostafa Eldeep <mostafa.eldeep89@gmail.com>
2019-08-06 23:16:14 +0200

    Add project files.

---
+++ database test/Controllers/clientsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using database_test.Models;
+
+namespace database_test.Controllers
+{
+    public class clientsController : Controller
+    {
+        private database_testContext db = new database_testContext();
+
+        // GET: clients
+        public ActionResult Index()
+        {
+            return View(db.clients.ToList());
+        }
+
+        // GET: clients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            clients clients = db.clients.Find(id);
+            if (clients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clients);
+        }
+      //  [Authorize rol]
+        // GET: clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Package,Phone")] clients clients)
+        {
+            if (ModelState.IsValid)
+            {
+                db.clients.Add(clients);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clients);
+        }
+
+        // GET: clients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            clients clients = db.clients.Find(id);
+            if (clients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clients);
+        }
+
+        // POST: clients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Package,Phone")] clients clients)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clients).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clients);
+        }
+
+        // GET: clients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            clients clients = db.clients.Find(id);
+            if (clients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clients);
+        }
+
+        // POST: clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            clients clients = db.clients.Find(id);
+            db.clients.Remove(clients);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ database test/Models/emp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace database_test.Models
+{
+    public class emp
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Job { get; set; }
+
+
+    }
+}
